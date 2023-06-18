4b735957bbad90f06ebf63fe4eeb431a814ea647
Alex <3697425+alexanderussell@users.noreply.github.com>
2023-06-18 22:27:42 +0000

    Jun 18, 2023, 5:27 PM

--- src/components/offerings.js
+++ src/components/offerings.js
@@ -2,11 +2,19 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 
-
+const Offering = () => (
+  <article>
+    image
+    <label>label</label>
+    <h2>Name</h2>
+    <caption>1 week &dot; $5000</caption>
+    <p>Align on business objectives and test key assumptions that must be true for your idea to succeed in the market.</p>
+  </article>
+)
 
 const Offerings = () => (
   <div>
-    Offerings
+    <Offering />
   </div>
 )
 
