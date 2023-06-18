98b64da7bb31feee2d0e82642fcaf63983752963
Alex <3697425+alexanderussell@users.noreply.github.com>
2023-06-18 21:37:15 +0000

    Jun 18, 2023, 4:37 PM

---
+++ src/components/clients.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const navLinks = [
+  { text: "home", url: "/", badge: false},
+  { text: "TypeScript", url: "using-typescript" },
+]
+
+const Clients = () => (
+  <div>
+    Clients
+  </div>
+)
+
+export default Clients
