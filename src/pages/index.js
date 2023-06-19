9e0243daa8b056491c0b61b40663c898e6801166
Alex <3697425+alexanderussell@users.noreply.github.com>
2023-06-19 01:24:58 +0000

    Jun 18, 2023, 8:24 PM

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,7 +67,7 @@ const IndexPage = () => (
     </div> */}
 
     <div className="container mx-auto px-container">
-      <h1 className="mt-10 text-3xl text-slate-900 font-bold md:mt-14 lg:mt-20">
+      <h1 className="mt-10 text-4xl text-slate-900 font-bold md:mt-14 lg:mt-20">
         <span className="mr-1 inline-block transition-all duration-500 ease-motion">
           We're a fully remote digital design and engineering collective. 
           <br />
