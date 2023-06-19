8da582ebdcbf2671da790a2551153a1933285580
Alex <3697425+alexanderussell@users.noreply.github.com>
2023-06-19 01:18:55 +0000

    Jun 18, 2023, 8:18 PM

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,13 +68,8 @@ const IndexPage = () => (
 
     <div className="container mx-auto px-container">
       <h1 className="mt-10 text-5xl font-bold md:mt-14 lg:mt-20">
-        <span className="mr-1 inline-block transition-all duration-500 ease-motion">Think.</span> 
-        <span className="mr-1 inline-block transition-all duration-500 ease-motion">Design.</span> 
-        <br />
-        <span className="mr-1 inline-block transition-all duration-500 ease-motion">Develop.</span>
-        <span className="mr-1 inline-block transition-all duration-500 ease-motion">Launch.</span> 
-        <br />
-        <span class="mr-1 inline-block transition-all duration-500 ease-motion text-foreground-secondary">Repeat.</span> 
+        <span className="mr-1 inline-block transition-all duration-500 ease-motion">We're a fully remote digital design and engineering collective. We believe in a partnership-first approach to design and work to bring your project from zero to one, and beyond.
+</span> 
       </h1>
     </div>
 
